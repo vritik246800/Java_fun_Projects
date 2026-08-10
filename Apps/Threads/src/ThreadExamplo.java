29d362badd44cc858d293913ddd0fffbf50d75e9
vritik246800 <vritikvalabdas@gmail.com>
2026-08-10 10:00:28 +0200

    update: Apps/Threads/src/ThreadExamplo.java

--- Apps/Threads/src/ThreadExamplo.java
+++ Apps/Threads/src/ThreadExamplo.java
@@ -1,5 +1,22 @@
 
-public class ThreadExamplo {
+class ThreadExamplo extends Thread {
 
+	public void run()
+	{
+		IO.println("Resultado: "+calc());
+	 	IO.println("O thread ja terminou !");
+	}
 	
+	int calc()
+	{
+		final int V1=8,V2=15,V3=6,V4=2;
+		
+		return (V1+V2+V3)*V4;
+	}
+
+	void main()
+	{
+		ThreadExamplo mt=new ThreadExamplo();
+		mt.start();
+	}
 }
